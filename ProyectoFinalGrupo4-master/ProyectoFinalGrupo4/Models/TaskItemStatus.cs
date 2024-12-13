376923dc89cd58bcb239cdc5efe5a319f26ff5b7
JAGM89 <joel.ag2004@gmail.com>
2024-12-12 22:54:27 -0600

    actualizacion en el model y creacion de la propiedades

--- ProyectoFinalGrupo4-master/ProyectoFinalGrupo4/Models/TaskItemStatus.cs
+++ ProyectoFinalGrupo4-master/ProyectoFinalGrupo4/Models/TaskItemStatus.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SQLite;
+
 
 namespace ProyectoFinalGrupo4.Models
 {
-    internal class TaskItemStatus
+    public enum TaskItemStatus
     {
+        PorHacer,
+        EnProceso,
+        Finalizada
+    }
+
+    [Table("Tasks")]
+    public partial class TaskItem: ObservableObject
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [ObservableProperty]
+        private string _title;
+
+        [ObservableProperty]
+        private string _description;
+
+        [ObservableProperty]
+        private TaskItemStatus _status;
+
+        [ObservableProperty]
+        private DateTime _CreatedDate;
+
+        public int StatusIndex
+        { 
+            get=> (int)_status; 
+            set=> _status= (TaskItemStatus)value;
+        }
     }
 }
+;
